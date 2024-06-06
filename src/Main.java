212f9102ae17868bbed518cf3a469a298cfb1ca9
Carlos Eduardo <carloseduardoevang@gmail.com>
2024-06-06 18:53:10 -0300

    feat: classes mentoria e curso criado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição do curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Descrição do curso Python");
+        curso2.setCargaHoraria(10);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("descrição mentoria de java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
+
     }
 }
